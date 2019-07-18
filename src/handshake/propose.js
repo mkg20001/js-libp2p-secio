cda8cb83f4b7ecebad413b02e0cdb140a97ddd0d
Maciej Krüger <mkg20001@gmail.com>
2019-07-18 14:27:55 +0200

    feat: rewrite propose

--- src/handshake/propose.js
+++ src/handshake/propose.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const debug = require('debug')
-const waterfall = require('async/waterfall')
 
 const support = require('../support')
 const crypto = require('./crypto')
@@ -11,25 +10,18 @@ log.error = debug('libp2p:secio:error')
 
 // step 1. Propose
 // -- propose cipher suite + send pubkeys + nonce
-module.exports = function propose (state, callback) {
+module.exports = async function propose (state) {
   log('1. propose - start')
 
   log('1. propose - writing proposal')
   support.write(state, crypto.createProposal(state))
 
-  waterfall([
-    (cb) => support.read(state.shake, cb),
-    (msg, cb) => {
-      log('1. propose - reading proposal', msg)
-      crypto.identify(state, msg, cb)
-    },
-    (cb) => crypto.selectProtocols(state, cb)
-  ], (err) => {
-    if (err) {
-      return callback(err)
-    }
+  log('1. propose - reading proposal')
+  const msg = await support.read(state.shake)
+  log('1. propose - read proposal', msg)
 
-    log('1. propose - finish')
-    callback()
-  })
+  await crypto.identify(state, msg)
+  await crypto.selectProtocols(state)
+
+  log('1. propose - finish')
 }
