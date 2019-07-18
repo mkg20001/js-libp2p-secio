05b62f4e5ef38f608f0725057c4a420d7cccb839
Maciej Krüger <mkg20001@gmail.com>
2019-07-18 14:31:56 +0200

    feat: rewrite exchange

--- src/handshake/exchange.js
+++ src/handshake/exchange.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const debug = require('debug')
-const waterfall = require('async/waterfall')
 
 const support = require('../support')
 const crypto = require('./crypto')
@@ -11,25 +10,18 @@ log.error = debug('libp2p:secio:error')
 
 // step 2. Exchange
 // -- exchange (signed) ephemeral keys. verify signatures.
-module.exports = function exchange (state, callback) {
+module.exports = async function exchange (state) {
   log('2. exchange - start')
 
   log('2. exchange - writing exchange')
-  waterfall([
-    (cb) => crypto.createExchange(state, cb),
-    (ex, cb) => {
-      support.write(state, ex)
-      support.read(state.shake, cb)
-    },
-    (msg, cb) => {
-      log('2. exchange - reading exchange')
-      crypto.verify(state, msg, cb)
-    },
-    (cb) => crypto.generateKeys(state, cb)
-  ], (err) => {
-    if (err) { return callback(err) }
+  const ex = await crypto.createExchange(state)
 
-    log('2. exchange - finish')
-    callback()
-  })
+  support.write(state, ex)
+  const msg = await support.read(state.shake)
+
+  log('2. exchange - reading exchange')
+  await crypto.verify(state, msg)
+
+  await crypto.generateKeys(state)
+  log('2. exchange - finish')
 }
